fd9bb05802ef5ad0e49d6f899b71389ae4377c90
Katarina Uvanovic <katarina.uvanovic@outlook.com>
2020-05-02 19:54:38 +0100

    two tetris blocks added

--- js/app.js
+++ js/app.js
@@ -15,9 +15,35 @@ document.addEventListener('DomContentLoaded', () => {
         [0, width, width+1, width*2+1],
         [width+1, width+2, width*2, width*2+1],
         [0, width, width+1, width*2+1],
-        [width+1, width+2, width*2, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
     ]
+
+    const tTetromino = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ]
+
+    const oTetromino = [
+        [0,1, width, width+1],
+        [0,1, width, width+1],
+        [0,1, width, width+1],
+        [0,1, width, width+1]
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+    ]
+
+    const Tetromiones = [Tetromino, zTetromino, tTetromino, oTetromino, iTetromino]
     
+    //randomly select Tetromino
+    
+
 })
 
 
