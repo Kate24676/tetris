6eaed9ede20ca9cb646f8a7e36b64e40f2661c0a
Katarina Uvanovic <katarina.uvanovic@outlook.com>
2020-05-02 19:14:52 +0100

    two tetris blocks added

--- js/app.js
+++ js/app.js
@@ -2,4 +2,22 @@ document.addEventListener('DomContentLoaded', () => {
 
     const width = 10
     const height = 20
+
+    //The Tetrimonoes
+    const  Tetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const zTetromino = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+    ]
+    
 })
+
+
