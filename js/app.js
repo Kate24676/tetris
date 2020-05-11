cdc0f1f0cd93b9c528b2b09c49cefab00c4f28b7
Katarina Uvanovic <katarina.uvanovic@outlook.com>
2020-05-11 23:22:11 +0100

    add score function added

--- js/app.js
+++ js/app.js
@@ -190,6 +190,23 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    //add score
+    function addScore() {
+        for (currentIndex = 0; currentIndex < 199; currentIndex += width) {
+            const row = [currentIndex, currentIndex+1, currentIndex+2, currentIndex+3, currentIndex+4, currentIndex+5, currentIndex+6, currentIndex+7, currentIndex+8, currentIndex+9]
+
+            if(row.every(index => squares[index].classList.contains('block2'))) {
+                score += 10
+                lines +=1
+                scoreDisplay.innerHTML = score
+                lines.innerHTML = lines
+                row.forEach(index => {
+                    squares[index].classList.remove('block2') || squares[index].classList.remove('block')
+                })
+
+            }
+        }
+    }
 
 })
 
